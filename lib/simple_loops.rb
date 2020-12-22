bb075a911c0b39873c70d485bfd42d18e50c02f8
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-22 04:02:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -31,7 +31,7 @@ output_array(array)
 
 def return_string_array(array)
   count = 0 
-  new_array = [1, 2, 3, 4, 5]
+  new_array = ["1", "2", "3", "4", "5"]
   while count < array.length do
     new_array.push(array[count].to_s)
     count += 1 
