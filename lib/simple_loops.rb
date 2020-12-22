57e605c15d1fe4410f8791838b92138c29337bc9
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-22 02:39:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
 # Write your methods here
+
+
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+loop_message_five_times("Hello World.")
+
+def loop_message_n_times(message, num = 5)
+    counter = 0 
+    while counter = num
+    puts message
+    counter += 1
+  end
+end
+loop_message_n_times("Hello Moon", 5)
